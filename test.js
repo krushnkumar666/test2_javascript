a2d9b5950caf574c6667ff689a42bc3ca00f7bc0
Krushnkumar333 <65840834+krushnkumar666@users.noreply.github.com>
2024-02-24 13:51:38 +0530

    updates

--- test.js
+++ test.js
@@ -3,18 +3,20 @@
 // String = “Hi, My name is Awdiz, I  am located at Vashi.” 
 
 // Output :  { Hi : 1, My : 1, name : 1, is : 1 …   }
-
-function uniqueWord(str){
+function uniqueWord(str) {
     let words = str.split(' ');
     let result = {};
 
-    for(let word of words){
-        if(!result[word]){
-            result[word]=1
-        }else{
+    for (let word of words) {
+        word = word.replace(/[,.]/g, '');
+
+        if (!result[word]) {
+            result[word] = 1;
+        } else {
             result[word]++;
         }
     }
     return result;
 }
+
 console.log(uniqueWord("Hi, My name is Awdiz, I am located at Vashi."));
