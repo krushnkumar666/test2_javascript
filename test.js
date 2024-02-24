c87d236db28f785bfd17bf363c3af995fef53017
Krushnkumar333 <65840834+krushnkumar666@users.noreply.github.com>
2024-02-24 13:19:55 +0530

    updates

---
+++ test.js
@@ -0,0 +1,20 @@
+// Q. Create a function that takes a string and returns a count of unique words. (, . are not words)
+
+// String = “Hi, My name is Awdiz, I  am located at Vashi.” 
+
+// Output :  { Hi : 1, My : 1, name : 1, is : 1 …   }
+
+function uniqueWord(str){
+    let words = str.split(' ');
+    let result = {};
+
+    for(let word of words){
+        if(!result[word]){
+            result[word]=1
+        }else{
+            result[word]++;
+        }
+    }
+    return result;
+}
+console.log(uniqueWord("Hi, My name is Awdiz, I am located at Vashi."));
